c228df28617c4f4743238b3bd7235c254879bdcb
merceg1 <118733323+merceg1@users.noreply.github.com>
2023-03-21 12:40:11 +0100

    config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    testIsolation: false,
+    baseUrl: "http://trserver:xxxx",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
