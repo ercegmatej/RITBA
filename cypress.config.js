8f799c98532544aea0d1049d92d5acbc79fc3f05
merceg1 <118733323+merceg1@users.noreply.github.com>
2023-04-21 15:24:41 +0200

    T1, commands

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     testIsolation: false,
-    baseUrl: "http://trserver:4321/",
+    baseUrl: "http://trserver:4321/login",
     viewportHeight: 1080,
     viewportWidth: 1920,
     chromeWebSecurity: false,
